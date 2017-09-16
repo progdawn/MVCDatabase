12fa4fa02814b4045317e482e936c600db06037a
Dawn Myers <progdawn@gmail.com>
2017-09-16 13:55:08 -0500

    Initial Commit
    Initial Commit

---
+++ MVCDatabase/Controllers/EmployeeController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using MVCDatabase.Models;
+
+namespace MVCDatabase.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        SqlConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["demodb"].ConnectionString.ToString());
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            List<Employee> emplist;
+            try
+            {
+                dbcon.Open();
+                emplist = Employee.GetEmployeeList(dbcon, "");
+                dbcon.Close();
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+            return View(emplist);
+        }
+
+        public ActionResult Create()
+        {
+            Employee emp = new Employee();
+            return View(emp);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    dbcon.Open();
+                    int intresult = Employee.CUDEmployee(dbcon, "create", emp);
+                    dbcon.Close();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex) { throw new Exception(ex.Message); }
+            }
+            ViewBag.errormsg = "Invalid data in Create Post action method";
+            return View("Error");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id.HasValue && (id.Value > 0 && id.Value <= 99))
+            {
+                try
+                {
+                    dbcon.Open();
+                    Employee emp = Employee.GetEmployeeSingle(dbcon, id.Value);
+                    dbcon.Close();
+                    if (emp.EmpId > 0) return View(emp);
+                }
+                catch (Exception ex) { throw new Exception(ex.Message); }
+            }
+            ViewBag.errormsg = "Invalid data in the Edit Page";
+            return View("Error");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    dbcon.Open();
+                    int intresult = Employee.CUDEmployee(dbcon, "update", emp);
+                    dbcon.Close();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex) { throw new Exception(ex.Message); }
+            }
+            ViewBag.errormsg = "Invalid data in Edit Post action method";
+            return View("Error");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id.HasValue && (id.Value > 0 && id.Value <= 99))
+            {
+                try
+                {
+                    dbcon.Open();
+                    Employee emp = Employee.GetEmployeeSingle(dbcon, id.Value);
+                    dbcon.Close();
+                    if (emp.EmpId > 0) return View(emp);
+                }
+                catch (Exception ex) { throw new Exception(ex.Message); }
+            }
+            ViewBag.errormsg = "Invalid data in the Delete Page";
+            return View("Error");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, FormCollection fc)
+        {
+            Employee emp = new Models.Employee();
+            emp.EmpId = id;
+            try
+            {
+                dbcon.Open();
+                int intresult = Employee.CUDEmployee(dbcon, "delete", emp);
+                dbcon.Close();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+        }
+    }
+}
---
+++ MVCDatabase/Models/Employee.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCDatabase.Models
+{
+    public class Employee
+    {
+        [Required, Key, Range(minimum: 1, maximum: 99)]
+        public int EmpId { get; set; }
+        [Required, MaxLength(20)]
+        public string LastName { get; set; }
+        [RegularExpression("^[1-9][0]$", ErrorMessage = "Dept Id is not valid")]
+        public int DeptId { get; set; }
+        [Required, Range(minimum: 500, maximum: 2500)]
+        public decimal Salary { get; set; }
+
+        public static Employee GetEmployeeSingle(SqlConnection dbcon, int id)
+        {
+            Employee obj = new Employee();
+            string strsql = "select * from Employee where EmpId = " + id;
+            SqlCommand cmd = new SqlCommand(strsql, dbcon);
+            SqlDataReader myReader;
+            myReader = cmd.ExecuteReader();
+            if (myReader.HasRows)
+            {
+                myReader.Read();
+                obj.EmpId = Convert.ToInt32(myReader["EmpId"].ToString());
+                obj.LastName = myReader["LastName"].ToString();
+                obj.DeptId = Convert.ToInt32(myReader["DeptId"].ToString());
+                obj.Salary = Convert.ToDecimal(myReader["Salary"].ToString());
+            }
+            myReader.Close();
+            cmd.Dispose();
+            return obj;
+        }
+
+        public static List<Employee> GetEmployeeList(SqlConnection dbcon, string SqlClause)
+        {
+            List<Employee> itemlist = new List<Employee>();
+            string strsql = "select * from Employee " + SqlClause;
+            SqlCommand cmd = new SqlCommand(strsql, dbcon);
+            SqlDataReader myReader;
+            myReader = cmd.ExecuteReader();
+            while (myReader.Read())
+            {
+                Employee obj = new Employee();
+                obj.EmpId = Convert.ToInt32(myReader["EmpId"].ToString());
+                obj.LastName = myReader["LastName"].ToString();
+                obj.DeptId = Convert.ToInt32(myReader["DeptId"].ToString());
+                obj.Salary = Convert.ToDecimal(myReader["Salary"].ToString());
+                itemlist.Add(obj);
+            }
+            myReader.Close();
+            cmd.Dispose();
+            return itemlist;
+        }
+
+        public static int CUDEmployee(SqlConnection dbcon, string CUDAction, Employee obj)
+        {
+            SqlCommand cmd = new SqlCommand();
+            if (CUDAction == "create")
+            {
+                cmd.CommandText = "insert into Employee " +
+                "Values (@EmpId,@LastName,@DeptId,@Salary)";
+                cmd.Parameters.AddWithValue("@EmpId", SqlDbType.Int).Value = obj.EmpId;
+                cmd.Parameters.AddWithValue("@LastName", SqlDbType.VarChar).Value =
+                obj.LastName;
+                cmd.Parameters.AddWithValue("@DeptId", SqlDbType.Int).Value = obj.DeptId;
+                cmd.Parameters.AddWithValue("@Salary", SqlDbType.Decimal).Value = obj.Salary;
+            }
+            else if (CUDAction == "update")
+            {
+                cmd.CommandText = "update Employee set LastName = @LastName," +
+                "DeptId = @DeptId,Salary = @Salary Where EmpId = @EmpId";
+                cmd.Parameters.AddWithValue("@LastName", SqlDbType.VarChar).Value =
+                obj.LastName;
+                cmd.Parameters.AddWithValue("@DeptId", SqlDbType.Int).Value = obj.DeptId;
+                cmd.Parameters.AddWithValue("@Salary", SqlDbType.Decimal).Value = obj.Salary;
+                cmd.Parameters.AddWithValue("@EmpId", SqlDbType.Int).Value = obj.EmpId;
+            }
+            else if (CUDAction == "delete")
+            {
+                cmd.CommandText = "delete Employee where EmpId = @EmpId";
+                cmd.Parameters.AddWithValue("@EmpId", SqlDbType.Int).Value = obj.EmpId;
+            }
+            cmd.Connection = dbcon;
+            int intResult = cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            return intResult;
+        }
+    }
+}
